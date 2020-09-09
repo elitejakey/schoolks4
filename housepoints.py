8bca3d20bea75dd742ee6a8de56546a0e0ab2cc7
mrgamer8573 <63165208+mrgamer8573@users.noreply.github.com>
2020-09-09 16:56:25 +0100

    School House Points

---
+++ housepoints.py
@@ -0,0 +1,10 @@
+print('The first class got 23 points\n The second class got 47 points\n The third class got 56 points\n The fourth class got 72 points.')
+
+answer = (23+47+56+73)
+
+print('The total number of house points is', answer)
+
+
+
+
+
