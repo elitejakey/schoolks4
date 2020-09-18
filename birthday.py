60f748563ef1e7a82ce611720f48f79667e5ef44
mrgamer8573 <63165208+mrgamer8573@users.noreply.github.com>
2020-09-18 17:53:46 +0100

    Birthday Days Challenge

---
+++ birthday.py
@@ -0,0 +1,7 @@
+name = input('What is your name?')
+
+days = int(input('How many days are there until your birthday?'))
+
+answer = (days*86400)
+
+print(f'Hey {name}, you told me that your birthday is in {days} days. This means it will be roughly {answer} seconds until then! How cool is that?')
