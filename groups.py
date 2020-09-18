98753154388db19f73b12bba130bc52e0b33fe3f
mrgamer8573 <63165208+mrgamer8573@users.noreply.github.com>
2020-09-18 17:41:30 +0100

    Groups challenge

---
+++ groups.py
@@ -0,0 +1,6 @@
+children = int(input('How many children are there in a group?'))
+groups = int(input('How many groups are there?'))
+
+answer = (children*groups)
+
+print(f'There are {answer} children in the class')
