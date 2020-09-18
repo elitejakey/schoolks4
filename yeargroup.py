10436052955d5e7bc0d56f8e9a6b078761ecac2d
mrgamer8573 <63165208+mrgamer8573@users.noreply.github.com>
2020-09-18 17:46:28 +0100

    Year Group Challenge

---
+++ yeargroup.py
@@ -0,0 +1,7 @@
+name = input('What is your name?')
+
+schoolyear  = int(input('What school year are you in?'))
+
+answer = (schoolyear+1)
+
+print(f'Hey {name}, next year you will be in Year {answer}.')
