e30d86dde5fa98bf1eddaf7e17eb627d0e3fc8f4
mrgamer8573 <63165208+mrgamer8573@users.noreply.github.com>
2020-09-18 17:59:10 +0100

    Table Size Challenge

--- tablesize.py
+++ tablesize.py
@@ -1,7 +1,9 @@
+name = input('What is your name?')
+
 length = int(input('What is the length of the table in centimeteres?'))
 
 width = int(input('What is the width of the table in centimeteres?'))
 
 answer = (length*width)
 
-print(f'The area of the table is {answer} cm sqaured!')
+print(f'Hey {name}, the area of the table is {answer} cm sqaured!')
