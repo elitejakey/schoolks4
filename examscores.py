54b7611160c538b45a5a92efdd7fc28a20ee8246
elitejakey <63165208+elitejakey@users.noreply.github.com>
2020-09-30 13:14:12 +0100

    Exam Scores Challenge

---
+++ examscores.py
@@ -0,0 +1,7 @@
+totalmarks = int(input('What is the total marks of all the students?'))
+totalstudents = int(input('How many students were there?'))
+
+answer =(totalmarks / totalstudents)
+finalanswer = int(round(answer))
+
+print('The average mark was', finalanswer)
