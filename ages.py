ef0352ff492076d6436463666505ef518c1af734
elitejakey <63165208+elitejakey@users.noreply.github.com>
2020-09-30 13:18:58 +0100

    Ages Challenge

---
+++ ages.py
@@ -0,0 +1,7 @@
+ages = int(input('What is the total ages of all the students?'))
+students = int(input('How many students are there?'))
+
+average = (ages / students)
+answer = int(float(average))
+
+print('The average age in the room is', answer)
