fdbb405906765d42ef5fb9412457e157e27aff42
elitejakey <63165208+elitejakey@users.noreply.github.com>
2020-10-02 17:13:51 +0100

    Favourite Subject Challenge

---
+++ favsubject.py
@@ -0,0 +1,10 @@
+subject = input("Please input the number which is your favourite subject out of the following: \n \n 1. English \n 2. Maths \n 3. Science \n 4. ICT \n \n Enter here: ")
+
+if subject == '1':
+    print("That's great!")
+if subject == '2':
+    print("Maths is cool!")
+if subject == '3':
+    print('Science is fun!')
+if subject == '4':
+    print('Me too!')
